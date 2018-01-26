e75e6f039a59856896a864e4db10af495fd1d64b
Marcos Lima <gabriel-m-x@hotmail.com>
2018-01-25 22:06:46 -0200

    colocando preco em sala e filme

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -14,7 +16,16 @@ public class Sessao {
 	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	private BigDecimal preco = new BigDecimal("0.00");
 	
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 	@ManyToOne
 	private Sala sala;
 	
@@ -31,6 +42,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public LocalTime getHorarioTermino() {
